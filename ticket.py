92bdc347e00cdda3d5b5cae24332afc190d1d994
oscar <Oscar.JohanssonElling@vitecsoftware.com # min mail>
2022-05-01 21:23:17 +0200

    forstog inte varför inte denna fil inte kom med i den tidigare commiten

---
+++ ticket.py
@@ -0,0 +1,15 @@
+import time
+import random
+
+class Ticket(object):
+    """ main data class """
+    def __init__(self):
+        assert buisinesValue > 0
+        self._buisinesValue = random.randrange(-10,10)
+        self._creationTime = time.time_ns()
+
+    def value(self):
+        return _buisinesValue
+
+    def time(self):
+        return _creationTime

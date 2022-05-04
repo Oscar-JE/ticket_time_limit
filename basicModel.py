f2a55ce8b533f194569b4fc3511bf161ea937dc8
oscar <Oscar.JohanssonElling@vitecsoftware.com # min mail>
2022-05-04 22:09:46 +0200

    provided functionality for comparison of tickets

--- basicModel.py
+++ basicModel.py
@@ -5,21 +5,21 @@ import random
 class Model(object):
     """docstring forBasic."""
     self.maximumSprintTickets = 15
-    self.teamsTicketCapacity = 6 
+    self.teamsTicketCapacity = 6
 
     def __init__():
-        self._ticket_list = []
-
+        self._tickets = []
+        self._ticketIndexs = [] # order of the tickets according to
+                                # their buisinesValue
     def _generateNumberOfTicketsNextSprint():
         # warning random not state check
         return random.randrange(maximumSprintTickets)
 
-    def generateTickets(n):
+    def _generateTickets(n):
         tickets = []
         for i in ragne(n):
             tickets.append(Ticket())
         return tickets
 
-
-    def calc(self):
-        return 4
+    def selectTickets(n):
+        # order a
--- ticket.py
+++ ticket.py
@@ -4,12 +4,17 @@ import random
 class Ticket(object):
     """ main data class """
     def __init__(self):
-        assert buisinesValue > 0
         self._buisinesValue = random.randrange(-10,10)
-        self._creationTime = time.time_ns()
 
     def value(self):
         return _buisinesValue
 
     def time(self):
         return _creationTime
+
+    # need the ability to compare tickets
+    def __le__(self,otherTicket):
+        return self._buisinesValue<= otherTicket._buisinesValue
+
+    def __gt__(self,ottherTicket):
+        return self._buisinesValue > otherTicket._buisinesValue

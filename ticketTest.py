d87851e04d37f09ad003d5fd1cd74004c0e67f8b
oscar <Oscar.JohanssonElling@vitecsoftware.com # min mail>
2022-05-07 10:24:01 +0200

    lagt till gitignore for den virtuella env

---
+++ ticketTest.py
@@ -0,0 +1,11 @@
+import unittest
+from .. import ticket
+
+class TestTicket(unittest.TestCase):
+    def test_bla(self):
+        myTicket = Ticket()
+        print(myTicket)
+
+
+if __name__ == "__main__":
+    unittest.main()

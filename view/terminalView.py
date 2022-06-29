ac119f0971fd3b0e27f7e1c4f3231eaf11b25d4c
oscar <Oscar.JohanssonElling@vitecsoftware.com # min mail>
2022-06-29 09:53:05 +0200

    en minimal terminal view

--- view/terminalView.py
+++ view/terminalView.py
@@ -1,7 +1,28 @@
 # terminal view
+import os
 
-class View(object):
-    """docstring for View."""
+def clearConsole():
+    rows = os.get_terminal_size()[1]
+    for i in range(rows-2):
+        print()
 
-    def visuilize(self,result):
+class View(object,):
+    """ Assumes that the value is sigle diget at the moment"""
+
+    def visuilize(self,result): # ska bytas ut
         print(result)
+
+    def choosenIndexRow(self, index):
+        str = " "
+        for i in range(index):
+            str += "   " # kommer bero på antaket siffror i resultatet kan vara ok för nu
+        marker = "v"
+        str += marker
+        print(str)
+
+    def draw(self, choosenIndex, result):
+        rows = os.get_terminal_size()[1]
+        for i in range(rows-2):
+            print()
+        self.choosenIndexRow(choosenIndex)
+        self.visuilize(result)
--- view/terminalViewTest.py
+++ view/terminalViewTest.py
@@ -1,10 +1,24 @@
 import unittest
 from terminalView import View
+import terminalView
 
 class TestTicket(unittest.TestCase):
+    @unittest.skip("onödigt construktorn gör inget specielt")
     def test_constructor(self):
         my_view = View()
         print(my_view)
+
+    def test_clearConsole(self):
+        print("clearConsole first line -----------------------")
+        terminalView.clearConsole()
+        print("second line ----------------------")
+
+    def test_draw(self):
+        result = [1,2,3,4]
+        choosenIndex = 1
+        my_view = View()
+        my_view.draw(choosenIndex,result)
+
 
 
 if __name__ == "__main__":
